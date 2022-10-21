c0dcb031ad71347446ac74e4b85d070074541bf9
Jared Nolen <Legodoghead@gmail.com>
2022-10-21 15:56:25 -0700

    Playtest build changes

--- Slime Garden/Assets/Scripts/PlayerController.cs
+++ Slime Garden/Assets/Scripts/PlayerController.cs
@@ -2,20 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public int cs = 0;  // The ammount of Congealed Slime held by the player
 
-    public Text csDisplay;
-
-    void Start()
-    {
-        
-    }
+    public TextMeshProUGUI csDisplay;
 
     void Update()
     {
-        csDisplay.text = "Monies: " + cs;
+        csDisplay.text = "Crystalized Slime: " + cs;
     }
 }
